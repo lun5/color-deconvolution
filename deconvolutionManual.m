a4f665ab82c9b7659236cb65a3b6f4b0d14a0efe
luongn <luongnth@gmail.com>
2014-04-09 16:46:45 -0400

    Keep stain vectors in RGB forms instead of OD

--- deconvolutionManual.m
+++ deconvolutionManual.m
@@ -1,4 +1,4 @@
-function [ purple_manual,pink_manual ] = deconvolutionManual( imname, datadir, resultdir, options,  varargin)
+function [ purple_manual_rgb,pink_manual_rgb ] = deconvolutionManual( imname, datadir, resultdir, options,  varargin)
 %deconvolution_man: manually deconvole colors by selecting regions that are
 %most purple and pink respectively
 defaultopt = struct('PlotResults','on',...
@@ -23,19 +23,21 @@ figure; imshow(raw_image);zoom('on'); zoom(2);
 rect = getrect; rect = floor(rect);
 purple_image_crop = imcrop(raw_image,rect);
 purple_od = rgb2od(purple_image_crop); % convert to OD
-purple_manual = mean(purple_od,2);
+purple_manual_od = mean(purple_od,2);
+purple_manual_rgb = mean(raw2rgb(purple_image_crop),2);
 %
 disp('manually select pink stain vector...');
 rect = getrect; rect = floor(rect);
 pink_image_crop = imcrop(raw_image,rect);
 pink_od = rgb2od(pink_image_crop); % convert to OD
-pink_manual = mean(pink_od,2);
+pink_manual_od = mean(pink_od,2);
+pink_manual_rgb = mean(raw2rgb(pink_image_crop),2);
 %
 disp('calculate optical density tuple ...');
 calculate_optical_density
 %
 disp('deconvole image by manually selected stain vectors...')
-stain_mat_man = [purple_manual pink_manual]; % do i need to normalize this?
+stain_mat_man = [purple_manual_od pink_manual_od]; % do i need to normalize this?
 saturation_mat_man = pinv(stain_mat_man)*opticalDensity;
 purple_stain_rgb = stainvec2rgb(stain_mat_man(:,1),saturation_mat_man(1,:),xsize,ysize);
 pink_stain_rgb = stainvec2rgb(stain_mat_man(:,2),saturation_mat_man(2,:),xsize,ysize);
