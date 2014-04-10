51458710ed0a62451c6308a8c4be0405f8d3d91a
luongn <luongnth@gmail.com>
2014-04-10 13:57:43 -0400

    input is flatten rgb image, not raw image

--- rgb2od.m
+++ rgb2od.m
@@ -1,11 +1,8 @@
-function [ od_image ] = rgb2od( raw_image )
+function [ od_image ] = rgb2od( rgb_image )
 %rgb2od convert from rgb space to optical density space 
-% INPUT:raw_image of size xsize x ysize x 3
+% INPUT:rgb_image of size 3 x (xsize x ysize), the flatten rgb image
 % OUPUT:od_image of size 3x number of pixels
-RGB = raw2rgb(raw_image); % R,G,B vector
-%RGBnorm = (RGB - repmat(min(RGB),3,1))./repmat(max(RGB) - min(RGB),3,1);
-%RGBnorm = normc(RGB);
-RGBnorm = RGB./255;
+RGBnorm = rgb_image./255;
 od_image = -log10(RGBnorm); % convert image to optical density values
 %od_image = -log(RGBnorm); % convert image to optical density values
 
