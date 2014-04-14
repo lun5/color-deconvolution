65fc008149fa2a329051f8524092fef7918b1623
luongn <luongnth@gmail.com>
2014-04-14 19:15:04 -0400

    Effect of changes in sigma_s

--- calculate_SIC.m
+++ calculate_SIC.m
@@ -10,22 +10,35 @@ rgb_image = rgb_image_whole(:,indx_pixel);
 
 options = struct('Normalize','on');
 
-mu_s = 4; sigma_s = 4;
-sic_image = rgb2sic( rgb_image, mu_s, sigma_s, options); 
-purple_stain_man_sic = rgb2sic(purple_manual_rgb);
-pink_stain_man_sic = rgb2sic(pink_manual_rgb);
-figure; scatter(sic_image(1,:),sic_image(2,:),20,rgb_image'./255,'filled');
-hold on
-h1=plot(pink_stain_man_sic(1),pink_stain_man_sic(2),'bs','MarkerSize',20,'MarkerFaceColor',...
-    pink_manual_rgb./255,'MarkerEdgeColor','k','LineWidth',3);
-
-h2=plot(purple_stain_man_sic(1),purple_stain_man_sic(2),'bo','MarkerSize',20,'MarkerFaceColor',...
-    purple_manual_rgb./255,'MarkerEdgeColor','k','LineWidth',3);
-xlabel('s1');
-ylabel('s2');
-legend([h1, h2],'manual pink stain', 'manual purple stain');
-line([-1 1],[0 0])
-line([0 0],[-1 1])
-hold off
-title('SIC 2D representation of hue');
-axis([-1 1 -1 1])
+% testing effect of different values of mu and sigma
+mu_s = 4;
+sigma_s = 4;
+sigma_s_vector = 1:1:10;
+for i = 1:length(sigma_s_vector)
+    sigma_s = sigma_s_vector(i);
+    sic_image = rgb2sic( rgb_image, mu_s, sigma_s, options); 
+    figure; scatter(sic_image(1,:),sic_image(2,:),20,rgb_image'./255,'filled');
+    hold on
+    xlabel('s1');ylabel('s2');
+    line([-1 1],[0 0]);line([0 0],[-1 1])
+    hold off
+    title('SIC 2D representation of hue'); axis([-1 1 -1 1]);
+end
+       
+% purple_stain_man_sic = rgb2sic(purple_manual_rgb);
+% pink_stain_man_sic = rgb2sic(pink_manual_rgb);
+% figure; scatter(sic_image(1,:),sic_image(2,:),20,rgb_image'./255,'filled');
+% hold on
+% h1=plot(pink_stain_man_sic(1),pink_stain_man_sic(2),'bs','MarkerSize',20,'MarkerFaceColor',...
+%     pink_manual_rgb./255,'MarkerEdgeColor','k','LineWidth',3);
+% 
+% h2=plot(purple_stain_man_sic(1),purple_stain_man_sic(2),'bo','MarkerSize',20,'MarkerFaceColor',...
+%     purple_manual_rgb./255,'MarkerEdgeColor','k','LineWidth',3);
+% legend([h1, h2],'manual pink stain', 'manual purple stain');
+% xlabel('s1');
+% ylabel('s2');
+% line([-1 1],[0 0])
+% line([0 0],[-1 1])
+% hold off
+% title('SIC 2D representation of hue');
+% axis([-1 1 -1 1])
