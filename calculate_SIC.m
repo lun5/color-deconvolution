b4dcadf918a274ac5a4d29c99c95da614a7db04b
luongn <luongnth@gmail.com>
2014-04-14 16:09:09 -0400

    Do not include normalization F in eq 8.4 Elmaraghi

--- calculate_SIC.m
+++ calculate_SIC.m
@@ -5,11 +5,15 @@ rotation_matrix = [1/sqrt(3) 1/sqrt(3) 1/sqrt(3); ...
     -1/sqrt(2) 1/sqrt(2) 0];
 rotated_coordinates = rotation_matrix*rgb_image;
 
-mu_s = 4; sigma_s = 4; % transition control parameters
-F = @(x, mu, sigma) 1-exp(- max(x - mu,0)/2/sigma^2);
-Fmatrix = F(sqrt(sum(rotated_coordinates(2:3,:).^2,1)),mu_s,sigma_s); 
-sic_image = repmat(Fmatrix,2,1).* rotated_coordinates(2:3,:)./...
-    repmat(1 + sqrt(sum(rotated_coordinates(2:3,:).^2,1)),2,1);
+% mu_s = 4; sigma_s = 4; % transition control parameters
+% F = @(x, mu, sigma) 1-exp(- max(x - mu,0)/2/sigma^2);
+% Fmatrix = F(sqrt(sum(rotated_coordinates(2:3,:).^2,1)),mu_s,sigma_s); 
+% sic_image = repmat(Fmatrix,2,1).* rotated_coordinates(2:3,:)./...
+%     repmat(1 + sqrt(sum(rotated_coordinates(2:3,:).^2,1)),2,1);
+
+% do not normalize
+sic_image = rotated_coordinates(2:3,:)./...
+     repmat(1 + sqrt(sum(rotated_coordinates(2:3,:).^2,1)),2,1);
 
 purple_stain_man_sic = rgb2sic(purple_manual_rgb);
 pink_stain_man_sic = rgb2sic(pink_manual_rgb);
