b94016d9e94d1bff2267d6909a30911ee24af9d7
luongn <luongnth@gmail.com>
2014-07-16 10:54:54 -0400

    use deconvolve_image inside this file

--- deconvolve_image_sequence.m
+++ deconvolve_image_sequence.m
@@ -9,16 +9,11 @@ min_stainvec_auto = zeros(3,numImages);
 max_stainvec_auto = zeros(3,numImages);
 purple_stainvec_man = zeros(3,numImages);
 pink_stainvec_man = zeros(3,numImages);
-for i = 1:numImages
-    raw_image = imread([datadir filesep imagepaths{i}]);
-    [ stain_mat_auto, saturation_mat_auto, project2svds, U] = deconvolutionSVD(imagepaths{i},workdir, filterOD, extremeCutoff);
-    min_stainvec_auto(:,i) = stain_mat_auto(:,1);
-    max_stainvec_auto(:,i) = stain_mat_auto(:,2);
-    [ purple_stain_man,pink_stain_man] = deconvolutionManual( imagepaths{i},workdir, filterOD );
-    purple_stainvec_man(:,i) = purple_stain_man;
-    pink_stainvec_man(:,i) = pink_stain_man;
-    plot_color_cloud;
+for i = 1:10 %numImages
+    imname = imagepaths{i};
+    deconvolve_image;
+    close all;
 end
 
-plotResults;
+%plotResults;
 
