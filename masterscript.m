c543f3b95300c00f79707e0c5698044444541131
luongn <luongnth@gmail.com>
2014-07-16 10:22:46 -0400

    not use deconvolve_image_sequence

--- masterscript.m
+++ masterscript.m
@@ -3,36 +3,33 @@
 % Demo code for color-deconvolution of H+E stained slides
 % Data: 30 slides of BC primary tumors from Dr. Lee
 % Method: Marc Macenko, Nancy E. Thomas, IEEE 2009
-
+% Method: NNMF
+% Method: SIC
+% New method: color opponency
+% May 26, 2014
+% LNguyen modified the method to show color opponency space instead of
+% saturation independent coordinates. 
 %%
-clearvars;
+%clearvars;
+addpath(pwd);
 disp('Directories of inputs and results')
-workdir = '/Users/lun5/Research/deconvolution'; 
+workdir = '/Users/lun5/Research/color_deconvolution'; 
+%datadir = uigetdir('*.tiff', 'Please select the image folder');
 datadir = fullfile(workdir, '20x_images','TIFF');
 resultdir = fullfile(workdir, 'results',datestr(now,'yymmdd'));
-
+close all;
 if ~exist(resultdir,'dir')
     mkdir(resultdir);
 end
 
-disp('Read the imput')
-imagepaths = getImInfo(datadir);
+disp('Read the input')
+imagepaths = getImInfo(datadir,'TIF');
 disp('Set parameters for automated de-convolution')
 extremeCutoff = 1;
 filterOD = 0.15;
 disp('Analyze the image set');
 
-% deconvolve_image_sequence;
-% ask user for name of image, input that into function
-% deconvole_image
-
-% debug flag: on off
-% plot flag: on off
-% put in options struct: parameter-method, flags
-% save image to powerpoint: 
-% take an optimal pink and an optimal purple. project pixels on the two
-% vectors, select the most purple and most pink based on the projection,
-% then calculate the saturation. 
+%deconvolve_image_sequence;
 
 prompt = 'Please enter the image name ';
 imname = input(prompt,'s');
