2f5b805b02945ac4f45b7b66dc01ec2c790d203e
luongn <luongnth@gmail.com>
2014-07-16 10:30:32 -0400

    modify input/output

--- deconvolve_image.m
+++ deconvolve_image.m
@@ -6,8 +6,23 @@
 raw_image = imread([datadir filesep imname]);
 options = struct('PlotResults','on',...
     'filterOD',0.15,...
-    'extremeCutoff',1);
-[ purple_stain_man,pink_stain_man] = deconvolutionManual( imname,datadir,resultdir);% options ); % why do I need workdir hmm
-[ stain_mat_svd, saturation_mat_svd, project2svds, U] = deconvolutionSVD(imname,datadir,resultdir);%,options);
-[ stain_mat_nnmf, saturation_mat_nnmf] = deconvolutionNNMF(imname,datadir,resultdir);%, options);
+    'extremeCutoff',1,...
+    'SavePlots','on');
+
+training3 = load([workdir filesep 'results' filesep '140507' filesep  'training_pink_purple.mat'],'training_data');
+%training_data = [training1.training_data training2.training_data];
+training_data = training3.training_data(:,1:4000);
+[U,D,V] = svd(training_data,0);
+rotation_matrix = [-U(:,1) U(:,2:3)]'; % this is correct one
+
+%[ purple_manual_rgb,pink_manual_rgb, stain_mat_man] = deconvolutionManual( imname,datadir,resultdir);% options ); 
+%[ stain1_svd, stain2_svd, saturation_mat_svd] = deconvolutionSVD(imname,datadir,resultdir);%,options);
+
+%options = struct('PlotResults','on',...
+%    'filterOD',0.15,'CropImage','off',...
+%    'extremeCutoff',1,'ColorSpace','SIC');
+%[ stain1_nnmf, stain2_nnmf, saturation_mat_nnmf] = deconvolutionNNMF(imname,datadir,resultdir, options);
 %plot_color_cloud;
+[ stain1_oppCol, stain2_oppCol, saturation_mat] = deconvolutionOppCol(imname,datadir,resultdir,rotation_matrix, options);
+% SIC color representation
+% calculate_SIC;
